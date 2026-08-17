11c2f2d1a67c8b70c6a79f23c91c1b1b9ccc85ef
Nikhil Rana <nikhilrana736333@gmail.com>
2026-08-17 13:02:02 +0545

    vite config issue fixing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,76 +1,61 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tsconfigPaths from "vite-tsconfig-paths";
-// import tailwindcss from "@tailwindcss/vite";
-// import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-// import path from "path";
-// import { nitro } from "nitro/vite";
-
-// export default defineConfig(({ command, mode }) => {
-//   return {
-//     css: { transformer: "lightningcss" },
-//     resolve: {
-//       alias: {
-//         "@": path.resolve(__dirname, "./src"),
-//       },
-//       dedupe: [
-//         "react",
-//         "react-dom",
-//         "react/jsx-runtime",
-//         "react/jsx-dev-runtime",
-//         "@tanstack/react-query",
-//         "@tanstack/query-core"
-//       ]
-//     },
-//     optimizeDeps: {
-//       include: ["react", "react-dom", "react-dom/client", "react/jsx-runtime", "react/jsx-dev-runtime"],
-//       ignoreOutdatedRequests: true
-//     },
-//     server: {
-//       host: true,
-//       port: 8080,
-//       strictPort: true,
-//       proxy: {
-//         "/api": {
-//           target: "http://localhost:3000",
-//           changeOrigin: true,
-//           secure: false,
-//         },
-//         "/uploads": {
-//           target: "http://localhost:3000",
-//           changeOrigin: true,
-//           secure: false,
-//         }
-//       }
-//     },
-//     plugins: [
-//       tailwindcss(),
-//       tsconfigPaths({ projects: ["./tsconfig.json"] }),
-//       tanstackStart({
-//         server: { entry: "server" },
-//         importProtection: {
-//           behavior: "error",
-//           client: {
-//             files: ["**/server/**"],
-//             specifiers: ["server-only"]
-//           }
-//         }
-//       }),
-//       command === "build" ? nitro({ defaultPreset: "cloudflare-module" }) : null,
-//       react(),
-//     ].filter(Boolean),
-//   };
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { nitro } from "vite-plugin-nitro";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import path from "path";
+import { nitro } from "nitro/vite";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
   return {
+    css: { transformer: "lightningcss" },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+      dedupe: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        "react/jsx-dev-runtime",
+        "@tanstack/react-query",
+        "@tanstack/query-core"
+      ]
+    },
+    optimizeDeps: {
+      include: ["react", "react-dom", "react-dom/client", "react/jsx-runtime", "react/jsx-dev-runtime"],
+      ignoreOutdatedRequests: true
+    },
+    server: {
+      host: true,
+      port: 8080,
+      strictPort: true,
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+          secure: false,
+        },
+        "/uploads": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    },
     plugins: [
-      TanStackRouterVite(),
+      tailwindcss( ),
+      tsconfigPaths({ projects: ["./tsconfig.json"] }),
+      tanstackStart({
+        server: { entry: "server" },
+        importProtection: {
+          behavior: "error",
+          client: {
+            files: ["**/server/**"],
+            specifiers: ["server-only"]
+          }
+        }
+      }),
       command === "build" ? nitro({ defaultPreset: process.env.NITRO_PRESET || "vercel" }) : null,
       react(),
     ].filter(Boolean),
